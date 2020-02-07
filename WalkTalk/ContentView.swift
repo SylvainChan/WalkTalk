7fc818d010324c5fff88b89afefb0c6062776996
Sylvain <me@sylvain.hk>
2020-02-07 20:53:22 +0800

    init commit

---
+++ WalkTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalkTalk
+//
+//  Created by Sylvain Chan on 7/2/2020.
+//  Copyright © 2020 Sylvain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
