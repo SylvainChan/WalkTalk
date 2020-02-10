af20a48dcbee680d8267c5c9731d0300a33610ee
Sylvain <me@sylvain.hk>
2020-02-10 14:57:48 +0800

    Add push notification

--- WalkTalk/AppDelegate.swift
+++ WalkTalk/AppDelegate.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var vc: UIViewController?
+    
+    var backgroundTask: UIBackgroundTaskIdentifier?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
@@ -25,8 +28,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.rootViewController = navVC
         self.window = window
         
+        // push
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (can, error) in
+            DispatchQueue.main.async {
+                if can {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
         
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        self.backgroundTask = application.beginBackgroundTask(expirationHandler: { [weak self] in
+            if let task = self?.backgroundTask {
+                application.endBackgroundTask(task)
+                self?.backgroundTask = UIBackgroundTaskIdentifier.invalid
+            }
+        })
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        if let task = self.backgroundTask {
+            application.endBackgroundTask(task)
+        }
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        NotificationCenter.default.post(name: NSNotification.Name.init(WTNotification.appWillTerminate.rawValue), object: nil)
+    }
+}
+
+// MARK: - push
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
+        UserConnection.pushToken = deviceTokenString
+        NotificationCenter.default.post(name: NSNotification.Name.init(WTNotification.pushTokenUpdated.rawValue), object: nil)
     }
 }
 
